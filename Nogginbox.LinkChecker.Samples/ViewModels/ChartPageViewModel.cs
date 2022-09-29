5b8df989cf074712999826cc53dedfd81997bcac
Richard Garside <code@nogginbox.co.uk>
2022-09-29 12:59:15 +0100

    Simple fake data

--- Nogginbox.LinkChecker.Samples/ViewModels/ChartPageViewModel.cs
+++ Nogginbox.LinkChecker.Samples/ViewModels/ChartPageViewModel.cs
@@ -4,6 +4,14 @@ namespace Nogginbox.LinkChecker.Samples.ViewModels;
 
 public class ChartPageViewModel : ObservableViewModelBase
 {
+    public ChartPageViewModel()
+    {
+        Lines.Add(new LineViewModel("OK", 0) { Value = 56 } );
+        Lines.Add(new LineViewModel("Not found", 1) { Value = 5 });
+        Lines.Add(new LineViewModel("Pending", 2) { Value = 12 });
+        Total = Lines.Sum(l => l.Value);
+    }
+
     public ObservableCollection<LineViewModel> Lines { get; set; } = new();
 
     public int Total
